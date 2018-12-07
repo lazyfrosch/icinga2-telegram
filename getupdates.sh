8066bbef0a495d65e35c889f4a618eceea463303
Markus Frosch <markus@lazyfrosch.de>
2018-12-07 16:06:28 +0100

    getupdates: Add getMe call

--- getupdates.sh
+++ getupdates.sh
@@ -29,8 +29,44 @@ if command -v python >/dev/null; then
   has_python=1
 fi
 
+base_url="https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}"
+
+curl() {
+  local url="$1"
+  shift
+
+  tempfile="$(mktemp)"
+  if ! command curl -LsS "${base_url}/$url" "$@" -o "$tempfile"; then
+    echo "curl failed with following output:" >&2
+    cat "$tempfile"
+    rm -f "$tempfile"
+  fi
+
+  echo "$tempfile"
+}
+
+output_json() {
+  local result_file="$1"
+  shift
+
+  echo "/* $(date -R) */"
+  if [ $has_python -eq 1 ]; then
+    python -m json.tool < "$result_file"
+  else
+    cat "$result_file"
+  fi
+
+  rm -f "$result_file"
+}
+
+get_me() {
+  result_file="$(curl getMe)"
+  output_json "$result_file"
+  echo
+}
+
 get_updates() {
-  url="https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/getUpdates"
+  url="${base_url}/getUpdates"
   curlopts=()
 
   if [ -n "$last_update_id" ]; then
@@ -40,26 +76,18 @@ get_updates() {
     curlopts+=(--data-urlencode "timeout=300")
   fi
 
-  tempfile="$(mktemp)"
-  if ! curl -LsS "${curlopts[@]}" "$url" -o "$tempfile"; then
-    echo "curl failed with following output:" >&2
-    cat "$tempfile"
-    rm -f "$tempfile"
-  fi
+  result_file="$(curl getUpdates "${curlopts[@]}")"
 
   if [ $long_polling -eq 1 ]; then
-    if [ "$(jq '.result | length' "$tempfile")" -gt 0 ] && last_update_id="$(jq ".result[-1].update_id" "$tempfile")"; then
+    if [ "$(jq '.result | length' "$result_file")" -gt 0 ] && last_update_id="$(jq ".result[-1].update_id" "$result_file")"; then
       ((last_update_id++))
     fi
   fi
 
-  echo "/* $(date -R) */"
-  if [ $has_python -eq 1 ]; then
-    python -m json.tool < "$tempfile"
-  else
-    cat "$tempfile"
-  fi
+  output_json "$result_file"
 }
+
+get_me
 
 get_updates
 
